8f588722808a8ac3e1ec0798ce2bf77c777b03a7
Vansh Sikka <141615610+vanshsikka1222@users.noreply.github.com>
2023-11-08 17:10:24 +0530

    Express Server

---
+++ 3.1 Express Server/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
+
+// 6 Steps to create an EXPRESS Server :
+
+/*
+1. Create Directory
+2. Create index.js file
+3. Initialise NPM
+4. Install the Express package
+5. Write Server application in index.js
+6. Start Server
+ */

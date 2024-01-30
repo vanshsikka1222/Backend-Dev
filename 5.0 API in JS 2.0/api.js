81c3aae68dc23bb19a859176d9bce0cf4c853d48
Vansh Sikka <141615610+vanshsikka1222@users.noreply.github.com>
2024-01-31 01:38:26 +0530

    API in JS 2.0

---
+++ 5.0 API in JS 2.0/api.js
@@ -0,0 +1,18 @@
+//API : Application Programming Interface
+//Diff Architectural Styles for APIs : GraphQL, SOAP, gRPC, REST:API...!!
+
+//REST API is most commonly used trending API
+
+//Rules of REST API
+//1. We use HTTP Protocol to access or communicate with REST API.
+//2. Will learn about GET, POST, PUT, PATCH, DELETE http Protocols
+
+// GET req through POSTMAN : https://api.wheretheiss.at/v1/satellites/25544
+//Give Latitude and Longitude of ISS Satellite in REAL time (LIVE)
+
+//RESPONSE
+
+/*
+{"name":"iss","id":25544,"latitude":26.146499124555,"longitude":-119.07954886074,"altitude":421.02770146167,"velocity":27581.545161775,"visibility":"daylight","footprint":4512.6882530057,"timestamp":1706558192,"daynum":2460339.3309259,"solar_lat":-17.899875043644,"solar_lon":244.13723508049,"units":"kilometers"}
+*/
+
